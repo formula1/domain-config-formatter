ad5083dc6d387aa29328ec6900aebf4ddc4d03c5
Samu <samtobia@gmail.com>
2022-01-30 18:44:12 -0800

    Code Cleanup: deep equal should be a util

--- src/programs/SIO/ProxyToGreenlock.ts
+++ src/programs/SIO/ProxyToGreenlock.ts
@@ -4,7 +4,7 @@ import { SIOCommand } from "./SIOCommand";
 import { formatJsonToGreenlock } from "../../formatters/greenlock";
 import { formatJsonToProxy } from "../../formatters/proxy";
 import { proxyToGreenlock } from "../../formatters"
-import { includes } from "../../util/array";
+import { deepEqual } from "../../util/json";
 
 export class ProxyToGreenlock extends SIOCommand {
   constructor(){
@@ -29,28 +29,4 @@ class JSONToGreenlock extends Transform {
       cb(e);
     }
   }
-}
-
-
-
-function deepEqual(a: JSON_Unknown,b: JSON_Unknown): boolean{
-  if(typeof a !== "object") return a === b;
-  if(typeof b !== "object") return false;
-  if(a === null) return a === b;
-  if(Array.isArray(a) && Array.isArray(b)){
-    if(a.length !== b.length) return false;
-    return !a.some((item)=>{
-      return !includes(b as Array<JSON_Unknown>, item, deepEqual);
-    })
-  }
-  if(Array.isArray(a) || Array.isArray(b)){
-    return false;
-  }
-  const akeys = Object.keys(a);
-  const bkeys = Object.keys(b);
-  if(akeys.length !== bkeys.length) return false;
-  return !akeys.some((key)=>{
-    if(!(key in b)) return true;
-    return !deepEqual(a[key], b[key]);
-  })
 }

9a353f19d7ad688ffa86c9ca8ee707cd89034977
Samu <samtobia@gmail.com>
2022-01-30 17:34:59 -0800

    Code Feat: Allowed includes to be more generic

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,8 +1,9 @@
 
 
-export function includes<T>(a: Array<T>, v: T): boolean{
-  for(var i = 0, l = a.length; i < l; i++){
-    if(a[i] === v) return true;
+export function includes<T, V>(ari: Array<T>, v: V, cb?: (a:T,b:V)=>boolean): boolean{
+  if(!cb) cb = (a, b)=>(a as T&V === b as T&V);
+  for(var i = 0, l = ari.length; i < l; i++){
+    if(cb(ari[i], v)) return true;
   }
   return false;
 }
